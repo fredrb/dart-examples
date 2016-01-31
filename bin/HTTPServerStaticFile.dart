2fcb7ceb23104680dcf6494b0c71484b34f278d0
Frederico <fred.rbittencourt@gmail.com>
2016-01-31 11:58:47 -0200

    serve static files

--- bin/HTTPServerStaticFile.dart
+++ bin/HTTPServerStaticFile.dart
@@ -9,6 +9,25 @@ startServer(String basePath) async {
   HttpServer server  = await HttpServer.bind('localhost', 3000);
 
   await for (HttpRequest req in server ) {
-    final String paht = req
+    final String path = req.uri.toFilePath();
+
+    // use regex
+    final String resultPath = path == '/' ? '/index.html' : path;
+    final File file = new File('${basePath}${resultPath}');
+
+    if (await file.exists()) {
+      try {
+        await file.openRead().pipe(req.response);
+      } catch(e) {
+        print(e);
+      }
+    } else {
+      _sendNotFound(req.response);
+    }
   }
+}
+
+main() {
+  File script = new File(Platform.script.toFilePath());
+  startServer(script.parent.path);
 }
