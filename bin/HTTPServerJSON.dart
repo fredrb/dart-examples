ae7869fcd0f6e46816ee4a5c60128c4d57402356
Frederico <fred.rbittencourt@gmail.com>
2016-02-01 02:06:24 -0200

    add correct content-type

--- bin/HTTPServerJSON.dart
+++ bin/HTTPServerJSON.dart
@@ -5,7 +5,9 @@ main() async {
   HttpServer server = await HttpServer.bind('localhost', 3000);
 
   await for (HttpRequest req in server) {
+
     req.response
+      ..headers.add("content-type", "application/json")
       ..write(JSON.encode({
         "message" : "Request received"
       }))
