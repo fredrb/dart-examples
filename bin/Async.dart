8c9e84fad63ab36d6ac1c6699d786134489f6149
Frederico <fred.rbittencourt@gmail.com>
2016-02-07 21:37:06 -0200

    .

--- bin/Async.dart
+++ bin/Async.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
-foo() async => new Future(() => new Timer(new Duration(seconds: 2), () => print("Foo completed")));
+foo() async => new Future(
+    () => new Timer(new Duration(seconds: 2), () => print("Foo completed")));
 
 main() async {
   foo();
